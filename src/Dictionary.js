edcf2378c5e7138e7b49be8f3445f93693debfd1
TLintott <traceyjlintott@hotmail.co.uk>
2023-10-11 11:45:41 +0100

    api integration added inc axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";  
 
 export default function Dictionary(){
-  let [keyword, setKeyword] = useState("");  
+  let [keyword, setKeyword] = useState(""); 
+  
+  function handResponse(response){
+    //console.log(response.data);
+  }
+
+
   function search(event){
     event.preventDefault();
-    alert(`searching for ${keyword} definition`);
+    
+
+let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=4b123t2611cd046f6e3o15d4a0230eef`;
+axios.get(apiUrl).then(handResponse);   
+//console.log(apiUrl);
   }
+
 
   function handleKeywordChange(event){
     setKeyword(event.target.value);
   }
-
 
 return <div className="Dictionary">
     <form onSubmit={search}>
