c026563ce803ad2be33edcc42b2af24637dbc94f
TLintott <traceyjlintott@hotmail.co.uk>
2023-10-11 10:41:05 +0100

    initial set up

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logotl from "./logotl.png";
+
+import './App.css';
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <img src={logotl} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+
