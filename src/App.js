cb47df883cde4049db226a072b4306cca622ce19
TLintott <traceyjlintott@hotmail.co.uk>
2023-10-11 11:19:15 +0100

    search engine added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logotl from "./logotl.png";
+import Dictionary from "./Dictionary";
 
 import './App.css';
 
@@ -6,9 +7,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
        <img src={logotl} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer"><small>Coded by TL</small></footer>
+      </div>
     </div>
   );
 }
