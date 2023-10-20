161ae5e3593c366f7a717a7423928374549df53d
TLintott <traceyjlintott@hotmail.co.uk>
2023-10-20 13:35:58 +0100

    removed line of  code

--- src/Photos.js
+++ src/Photos.js
@@ -11,7 +11,7 @@ return (
             //console.log(props.photo);
             return(
               <div className="col-sm-4" key={index}>
-                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                <a href={photo.src.original} target="_blank">
               <img src={photo.src.landscape} className="img-fluid" alt=""  />
               </a>
               </div>
